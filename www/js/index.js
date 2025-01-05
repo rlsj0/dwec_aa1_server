4977610da30006cd3f9af86f479f05197cb21bd4
rlsj0 <rodrileja@gmail.com>
2025-01-05 17:42:39 +0100

    Create method to add categories

--- www/js/index.js
+++ www/js/index.js
@@ -22,3 +22,18 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 // Remind: the fetch returns an array of objects
+
+function addCategory(category_name) {
+    fetch("http://localhost:3000/categories", {
+        method: "POST",
+        body: JSON.stringify({
+            "name": category_name,
+        }),
+        headers: {
+            "Content-type": "application/json",
+        },
+    })
+        .then((res) => res.json())
+        .then((json) => console.log(json))
+        .catch(() => console.log("Error"));
+}
