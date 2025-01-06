c9a23bc4e7bae06ef734c2969a8632bd0da118bb
rlsj0 <rodrileja@gmail.com>
2025-01-06 03:14:42 +0100

    implement addition of sites

--- www/js/add-site.js
+++ www/js/add-site.js
@@ -1,14 +1,18 @@
+let siteInput;
 let urlInput;
 let userInput;
 let passwordInput;
+let descriptionInput;
 let button;
 
 document.addEventListener("DOMContentLoaded", () => {
     // Declare variables
+    siteInput = document.getElementById("site-name");
     urlInput = document.getElementById("url-site");
     userInput = document.getElementById("user-site");
     passwordInput = document.getElementById("password-site");
     button = document.getElementById("saveButton");
+    descriptionInput = document.getElementById("description-site");
     // Hidden input: set value to category id
     const urlParameters = window.location.search;
     let params = new URLSearchParams(urlParameters);
@@ -20,6 +24,7 @@ document.addEventListener("DOMContentLoaded", () => {
 // Called from add-site.html
 function verifyForm() {
     if (
+        siteInput.value != "" &&
         urlInput.value != "" && userInput.value != "" &&
         passwordInput.value != ""
     ) {
@@ -27,4 +32,49 @@ function verifyForm() {
     } else {
         button.disabled = true;
     }
+}
+
+// Called from add-site.html
+function addSite() {
+    // Add site name and description
+    siteName = siteInput.value;
+    url = urlInput.value;
+    user = userInput.value;
+    password = passwordInput.value;
+    description = descriptionInput.value;
+    categoryID = document.getElementById("category-id").value;
+    console.log(siteName);
+    console.log(url);
+    console.log(user);
+    console.log(password);
+    console.log(description);
+
+    let fetchUrl = "http://localhost:3000/categories/" + categoryID;
+    fetch(fetchUrl, {
+        method: "POST",
+        body: JSON.stringify({
+            "name": siteName,
+            "url": url,
+            "user": user,
+            "password": password,
+            "description": description,
+        }),
+        headers: {
+            "Content-type": "application/json",
+        },
+    })
+        .then((res) => res.json())
+        .then((json) => console.log(json))
+        .catch(() => console.log("Error"));
+
+    // Clean the input fields to prevent double submit
+    cleanInput();
+}
+
+function cleanInput() {
+    siteInput.value = "";
+    userInput.value = "";
+    urlInput.value = "";
+    passwordInput.value = "";
+    descriptionInput.value = "";
 }
